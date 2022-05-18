c657e30de6bb96ac4ec7a9db362dc385337eedf9
Ibrhim Al Sweis <matrexnat9911@gmail.com>
2022-05-18 19:50:46 +0300

    create app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,  on_delete=models.CASCADE)
+    image = models.ImageField( upload_to='Users_profiles_images/%y/%m/%d')
+
+    def __str__(self):
+        return f'User Profile: {self.user.first_name} {self.user.last_name}'
+    ذ
